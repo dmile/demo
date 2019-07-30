be76366f6ca98f9db73c233f2d449c3f2bb82c75
Dmitry Lepskiy <lepsky.dmitry@gmail.com>
2019-07-30 22:56:22 +0200

    Updates

---
+++ precache-manifest.18b2de84c5bd81bf015b7a110e1861b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3c8c35cb4a9014010686bb043d9ab7",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "c1334030900c570ca760",
+    "url": "/demo/static/js/2.a0bff675.chunk.js"
+  },
+  {
+    "revision": "b11265b7b92dd217d285",
+    "url": "/demo/static/js/main.e36488a8.chunk.js"
+  },
+  {
+    "revision": "2f23047b078009b94f5f",
+    "url": "/demo/static/js/runtime~main.96f7b30f.js"
+  }
+]);
